c0592da3c7ef12741d004297fe0a90b18171c6da
khaivnhust <nguyenthekhaitg@gmail.com>
2025-11-18 13:29:08 +0700

    Implement 2.2: Classifier Member và Instance Member
    * Update DigitalVideoDisc.java
    
    * Reload Class astah file
    
    * Reload Class png file

--- AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
+++ AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
@@ -5,40 +5,55 @@ public class DigitalVideoDisc {
     private int length;
     private double cost;
 
-  
+
+    private static int nbDigitalVideoDiscs = 0;
+
+ 
+    private int id;
+
+
     public DigitalVideoDisc(String title) {
         this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
-    
     public DigitalVideoDisc(String category, String title, double cost) {
         this.category = category;
         this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
-  
     public DigitalVideoDisc(String director, String category, String title, double cost) {
         this.director = director;
         this.category = category;
         this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
-    
     public DigitalVideoDisc(String title, String category, String director, int length, double cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
-    
     public String getTitle() { return title; }
     public String getCategory() { return category; }
     public String getDirector() { return director; }
     public int getLength() { return length; }
     public double getCost() { return cost; }
-}
 
+    public int getId() { return id; }
+
+    public static int getNbDigitalVideoDiscs() { 
+        return nbDigitalVideoDiscs; 
+    }
+}
