b1ac9ec479be05a4ac617e51cf38e122f306d13f
khaivnhust <nguyenthekhaitg@gmail.com>
2025-11-18 21:39:29 +0700

    Update DigitalVideoDisc.java

--- AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
+++ AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
@@ -1,74 +1,14 @@
 package com.hust.kstn.models;
 
-public class DigitalVideoDisc {
+public class DigitalVideoDisc extends Disc {
 
-    private String title;
-    private String category;
-    private String director;
-    private int length;
-    private double cost;
-
- 
-    private static int nbDigitalVideoDiscs = 0;
-
-   
-    private int id;
-
-    
-    public DigitalVideoDisc(String title) {
-        this.title = title;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
+    public DigitalVideoDisc(String title, double cost, String category, String director, int length) {
+        super(title, cost, category, director, length);
     }
 
-    public DigitalVideoDisc(String category, String title, double cost) {
-        this.category = category;
-        this.title = title;
-        this.cost = cost;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
-    }
-
-    public DigitalVideoDiscc(String director, String category, String title, double cost) {
-        this.director = director;
-        this.category = category;
-        this.title = title;
-        this.cost = cost;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
-    }
-
-    public DigitalVideoDisc(String title, String category, String director, int length, double cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.length = length;
-        this.cost = cost;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
-    }
-
-    
-
-    public String getTitle() { return title; }
-    public String getCategory() { return category; }
-    public String getDirector() { return director; }
-    public int getLength() { return length; }
-    public double getCost() { return cost; }
-    public int getId() { return id; }
-
-    public static int getNbDigitalVideoDiscs() {
-        return nbDigitalVideoDiscs;
-    }
-
-  
     @Override
     public String toString() {
-        return "DVD[" + this.id + "]"
-                + " – " + this.title
-                + " – " + this.cost
-                + " – " + this.length
-                + " – " + this.category
-                + " – " + this.director;
+        return "DVD[" + id + "][" + title + "][" + cost + "][" + category + 
+               "][" + director + "][" + length + " min]";
     }
 }
